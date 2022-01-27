f619e4dfefd9c5f722678b2c73a91080d4e29cf1
Nabilbentamer <nabil.bentamer7@gmail.com>
2022-01-27 23:36:27 +0100

    add input component

---
+++ BarberShop-FrontEnd/src/components/Input.js
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components/native";
+
+import { View, TextInput } from "react-native";
+import { Icon } from "react-native-elements";
+
+const InputArea = styled.View`
+  background: #d3d3d3;
+  height: 40px;
+  margin-bottom: 8px;
+  padding: 0 16px;
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export default ({ IconName, placeholder }) => {
+  return (
+    <InputArea>
+      <Icon name={IconName} />
+      <TextInput
+        placeholder={placeholder}
+        placeholderTextColor="white"
+      ></TextInput>
+    </InputArea>
+  );
+};
