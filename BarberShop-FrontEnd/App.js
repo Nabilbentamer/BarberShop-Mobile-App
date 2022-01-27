fb5fb28f9f11398874a9ef90e82dfafb51dc35c9
Nabilbentamer <nabil.bentamer7@gmail.com>
2022-01-27 17:56:59 +0100

    add main Stack

--- BarberShop-FrontEnd/App.js
+++ BarberShop-FrontEnd/App.js
@@ -1,17 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-
+import React from "react";
 import MainStack from "./src/stack/MainStack";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
-  return <MainStack></MainStack>;
+  return (
+    <NavigationContainer>
+      <MainStack />
+    </NavigationContainer>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- BarberShop-FrontEnd/src/stack/MainStack.js
+++ BarberShop-FrontEnd/src/stack/MainStack.js
@@ -1,24 +1,33 @@
 import React from "react";
 
-import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import SplashScreen from "../pages/SplashScreen/index";
+import Login from "../pages/Login/index";
+import Signup from "../pages/SignUp/index";
 
 const Stack = createStackNavigator();
 
 const MainStack = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="SplaShScreen"
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name="SplashScreen" component={SplashScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator
+      initialRouteName="SplaShScreen"
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen
+        name="SplashScreen"
+        component={SplashScreen}
+        screenOptions={{ headerShown: true }}
+      />
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        screenOptions={{ headerShown: true }}
+      />
+      <Stack.Screen name="Signup" component={Signup} />
+    </Stack.Navigator>
   );
 };
 
