34dc336f3717321d57ebb7a9346507b27aa02ec5
Nabilbentamer <nabil.bentamer7@gmail.com>
2022-01-27 23:36:40 +0100

    add login page code

--- BarberShop-FrontEnd/src/pages/Login/index.js
+++ BarberShop-FrontEnd/src/pages/Login/index.js
@@ -1,12 +1,41 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { TextInput, View, Text } from "react-native";
 
-const login = () => {
+import Input from "../../components/Input";
+
+import * as S from "./style";
+
+const Login = () => {
   return (
-    <View>
-      <Text>This is Login Page</Text>
-    </View>
+    <S.Container>
+      <S.MainTitle>Welcome</S.MainTitle>
+      <S.MainTitle>Back</S.MainTitle>
+
+      <S.SubTitle>Sign in to Continue</S.SubTitle>
+
+      <S.InputArea>
+        <Input placeholder="Enter your Email adresse" IconName="mail" />
+        <Input placeholder="Enter your password" IconName="lock" />
+      </S.InputArea>
+
+      <S.textContainer>
+        <Text>Remember me</Text>
+        <Text>Forgot Password ? </Text>
+      </S.textContainer>
+
+      <S.SigninButton onPress={() => navigation.navigate("Signup")}>
+        <S.SignInText>Sign in</S.SignInText>
+      </S.SigninButton>
+
+      <S.SignFbButton onPress={() => navigation.navigate("Login")}>
+        <S.SignFbText>Sign in With Facebook </S.SignFbText>
+      </S.SignFbButton>
+
+      <View>
+        <Text>Don't Have an Account ? Sign up </Text>
+      </View>
+    </S.Container>
   );
 };
 
-export default login;
+export default Login;
---
+++ BarberShop-FrontEnd/src/pages/Login/style.js
@@ -0,0 +1,61 @@
+import React from "react";
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  padding: 90px 30px;
+  background-color: white;
+  flex: 1;
+`;
+export const MainTitle = styled.Text`
+  font-size: 30px;
+  font-weight: bold;
+`;
+
+export const SubTitle = styled.Text`
+  font-size: 16px;
+  color: gray;
+  margin-top: 10px;
+`;
+
+export const InputArea = styled.View`
+  width: 100%;
+  margin-top: 30px;
+  margin-bottom: 10px;
+`;
+
+export const SigninButton = styled.TouchableOpacity`
+  background: #232129;
+  border-color: #232129;
+  border-radius: 5px;
+  border-width: 2px;
+  height: 40px;
+  margin-bottom: 8px;
+  padding: 2px 16px;
+  width: 100%;
+  align-items: center;
+`;
+
+export const SignInText = styled.Text`
+  color: white;
+  font-size: 20px;
+`;
+
+export const SignFbButton = styled.TouchableOpacity`
+  background: white;
+  padding: 10px 20px;
+  border: 1px solid black;
+  border-radius: 15px;
+  align-items: center;
+  margin-top: 15px;
+`;
+
+export const SignFbText = styled.Text`
+  color: black;
+  font-size: 15px;
+`;
+export const textContainer = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 25px;
+`;
