b23249feb6ada42485f33ad08bb66a965432a80e
Nabilbentamer <nabil.bentamer7@gmail.com>
2022-01-28 23:28:11 +0100

    add redux for login

--- BarberShop-FrontEnd/src/pages/Login/index.js
+++ BarberShop-FrontEnd/src/pages/Login/index.js
@@ -1,40 +1,93 @@
 import React from "react";
-import { TextInput, View, Text } from "react-native";
+import { Alert, TextInput, View, Text } from "react-native";
 
 import Input from "../../components/Input";
 
 import * as S from "./style";
 
+import { Formik } from "formik";
+import * as yup from "yup";
+import { useDispatch } from "react-redux";
+import { useNavigation } from "@react-navigation/native";
+import * as authAction from "../../redux/actions/AuthActions";
+
+const formSchema = yup.object({
+  email: yup.string().email().required(),
+  password: yup.string().required().min(6),
+});
+
 const Login = () => {
+  const navigation = useNavigation();
+  const dispatch = useDispatch();
+
   return (
-    <S.Container>
-      <S.MainTitle>Welcome</S.MainTitle>
-      <S.MainTitle>Back</S.MainTitle>
+    <Formik
+      initialValues={{
+        email: "",
+        password: "",
+      }}
+      validationSchema={formSchema}
+      onSubmit={(values) =>
+        dispatch(authAction.loginUser(values))
+          .then((result) => {
+            if (result.success) {
+              navigation.navigate("Main");
+            } else {
+              Alert.alert(result.message);
+            }
+          })
+          .catch((err) => console.log(err))
+      }
+    >
+      {(props) => (
+        <S.Container>
+          <S.MainTitle>Welcome</S.MainTitle>
+          <S.MainTitle>Back</S.MainTitle>
 
-      <S.SubTitle>Sign in to Continue</S.SubTitle>
+          <S.SubTitle>Sign in to Continue</S.SubTitle>
 
-      <S.InputArea>
-        <Input placeholder="Enter your Email adresse" IconName="mail" />
-        <Input placeholder="Enter your password" IconName="lock" />
-      </S.InputArea>
+          <S.InputArea>
+            <Input
+              placeholder="Enter your Email adresse"
+              IconName="mail"
+              value={props.values.email}
+              onChangeText={props.handleChange("email")}
+              onBlur={props.handleBlur("email")}
+            />
+            <Text>{props.touched.email && props.errors.email}</Text>
+            <Input
+              placeholder="Enter your password"
+              IconName="lock"
+              value={props.values.password}
+              onChangeText={props.handleChange("password")}
+              onBlur={props.handleBlur("password")}
+            />
+            <Text>{props.touched.password && props.errors.password}</Text>
+          </S.InputArea>
 
-      <S.textContainer>
-        <Text>Remember me</Text>
-        <Text>Forgot Password ? </Text>
-      </S.textContainer>
+          <S.textContainer>
+            <Text>Remember me</Text>
+            <Text>Forgot Password ? </Text>
+          </S.textContainer>
 
-      <S.SigninButton onPress={() => navigation.navigate("Signup")}>
-        <S.SignInText>Sign in</S.SignInText>
-      </S.SigninButton>
+          <S.SigninButton
+            onPress={() => {
+              props.handleSubmit("formik");
+            }}
+          >
+            <S.SignInText>Sign in</S.SignInText>
+          </S.SigninButton>
 
-      <S.SignFbButton onPress={() => navigation.navigate("Login")}>
-        <S.SignFbText>Sign in With Facebook </S.SignFbText>
-      </S.SignFbButton>
+          <S.SignFbButton onPress={() => navigation.navigate("SplashScreen")}>
+            <S.SignFbText>Sign in With Facebook </S.SignFbText>
+          </S.SignFbButton>
 
-      <View>
-        <Text>Don't Have an Account ? Sign up </Text>
-      </View>
-    </S.Container>
+          <View>
+            <Text>Don't Have an Account ? Sign up </Text>
+          </View>
+        </S.Container>
+      )}
+    </Formik>
   );
 };
 
