176dcda479ccaddd5e935092524b03c8e92b008b
Nabilbentamer <nabil.bentamer7@gmail.com>
2022-02-06 00:23:02 +0100

    add a fake backend

---
+++ BarberShop-FrontEnd/src/Services/Barbers.js
@@ -0,0 +1,123 @@
+export const localBarbershops = [
+  {
+    name: "235th Barber",
+    image_url:
+      "https://static.lpnt.fr/images/2017/08/04/9635864lpaw-9641167-jpg_4467945.jpg",
+    categories: ["Cafe", "Bar"],
+    price: "$$",
+    reviews: 24,
+    rating: 4.5,
+    addresse: "Ali Abderrazzak Résidence Pavillon",
+    barbers: [
+      {
+        name: "Hamid",
+        photo: "https://i.imgur.com/GDtyJbb.jpg",
+        rating: 127,
+        location: "casablanca",
+      },
+      {
+        name: "Ali",
+        photo: "https://i.imgur.com/L73tVfv.jpg",
+        rating: 15,
+        location: "casablanca",
+      },
+      {
+        name: "Hassan",
+        photo: "https://i.imgur.com/6FeZyIu.jpg",
+        rating: 23,
+        location: "casablanca",
+      },
+    ],
+    user_reviews: [
+      {
+        name: "Fouad Simpson",
+        text: "Wonderful expérience in the Barbershop and good guys there",
+        photo: "https://i.imgur.com/j7J5kIM.jpg",
+        rating: 5,
+        time: "3 months ago",
+      },
+      {
+        name: "Mehdi Najihi",
+        text: "I came for a basic haircut. I had the least good one I can think of I could see he can do better, he just couldnt be bothered ?!",
+        photo:
+          "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80",
+        rating: 2,
+        time: "a year ago",
+      },
+    ],
+  },
+  {
+    name: "Miamar Barber Shop",
+    image_url:
+      "https://lh3.googleusercontent.com/p/AF1QipPJP4uem501hwfwQ5RceuibuLtc3ZbWPcBE1IMF=s1600-w400",
+    categories: ["Cafe", "Bar"],
+    price: "$$",
+    reviews: 31,
+    rating: 4.8,
+    addresse: "6B Résidence Miamar, Casablanca",
+    barbers: [
+      {
+        name: "Omar",
+        photo:
+          "https://images.squarespace-cdn.com/content/v1/562eb1a8e4b0cd8682c40651/1575483224032-8QDRU79RHGBUB5UDHT5P/caeron+boyce.jpg?format=1000w",
+        rating: 17,
+        location: "casablanca",
+      },
+      {
+        name: "Nabil",
+        photo:
+          "https://i.pinimg.com/originals/fd/34/ac/fd34aca5ab6f20f4c7b243303a89cb79.jpg",
+        rating: 15,
+        location: "casablanca",
+      },
+      {
+        name: "Anass",
+        photo:
+          "https://menshaircuts.com/wp-content/uploads/2021/01/how-to-style-short-hair-men-high-fade-pompadour-beard-683x1024.jpg",
+        rating: 23,
+        location: "casablanca",
+      },
+    ],
+    user_reviews: [
+      {
+        name: "Ali Elouallaji",
+        text: "It’s been 4years since I was searching for a barber that can make me happy and with Anwar I got the best cut from the best barber",
+        photo: "https://i.imgur.com/CEWJGlc.jpg",
+        rating: 5,
+        time: "6 months ago",
+      },
+      {
+        name: "junjie xu",
+        text: "every three weeks i go cutting my hair，the people ther are very nice，can easily understand what the style you want",
+        photo: "https://i.imgur.com/Lzs3rEE.jpg",
+        rating: 5,
+        time: "a year ago",
+      },
+      {
+        name: "Walid Zazzou",
+        text: "This is the second time I come here, I never know what haircut suits me, then I met anouar and his team, they made me satisfied about everything: the haircut, the service, the music, the smile! They are more than friendly! I advise anyone who wants a nice haircut experience to visit Anouar Barbershop in Gauthier 💚",
+        photo: "https://i.imgur.com/sEV3oVx.jpg",
+        rating: 5,
+        time: "a year ago",
+      },
+    ],
+  },
+  {
+    name: "Andory Barber Shop",
+    image_url:
+      "https://www.topjour.com/wp-content/uploads/2017/11/barber-shop-marrakech-820x420.jpg",
+    categories: ["Cafe", "Bar"],
+    price: "$$",
+    reviews: 1244,
+    rating: 3.7,
+  },
+  {
+    name: "Prosizo Barbershop",
+    image_url:
+      "https://mir-s3-cdn-cf.behance.net/project_modules/fs/515ff744490221.5813c8585021d.jpg",
+    categories: ["Indian", "Bar"],
+    price: "$$",
+    reviews: 700,
+    rating: 4.9,
+  },
+];
